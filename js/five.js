95c5ff75d27ee360ab06bd1edddd866731e22367
Ai01 <2911403289@qq.com>
2016-04-14 17:53:47 +0800

    five
    智能五子棋

---
+++ js/five.js
@@ -0,0 +1,249 @@
+window.onload = function() {
+	//over表示这局棋是否结束
+	var over = false;
+	//ui部分
+	//为了使已经落子的位置无法在落子所以需要确定棋盘状况，用二维数组表示棋盘
+	var chessBoard = [];
+	for (var i = 0; i < 15; i++) {
+		chessBoard[i] = [];
+		for (var j = 0; j < 15; j++) {
+			chessBoard[i][j] = 0;
+		}
+	}
+	//me用来确定黑白棋子
+	var me = true;
+	//canvas画布
+	var chess = document.getElementById("chess");
+	var context = chess.getContext("2d");
+	//画棋盘函数
+	function drawChessBoard() {
+		for (var i = 0; i < 15; i++) {
+			context.strokeStyle = "#BFBFBF";
+			//竖线
+			context.moveTo(15 + i * 30, 15);
+			context.lineTo(15 + i * 30, 435);
+			context.stroke();
+			//横线
+			context.moveTo(15, 15 + i * 30);
+			context.lineTo(435, 15 + i * 30);
+			context.stroke();
+		}
+	}
+	drawChessBoard();
+	//画棋子函数
+	function drawChessman(i, j, me) {
+		context.beginPath();
+		context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI, true);
+		context.closePath();
+		//让棋子中间显示有光泽，其实就是渐变色
+		var gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0);
+		//黑白不同的颜色
+		if (me) {
+			gradient.addColorStop(0, "#0A0A0A");
+			gradient.addColorStop(1, "#636766");
+		} else {
+			gradient.addColorStop(0, "#D1D1D1");
+			gradient.addColorStop(1, "#F9F9F9");
+		}
+		context.fillStyle = gradient;
+		context.fill();
+	}
+
+
+
+	//ai部分
+	//定义赢法数组wins是一个三维数组，前两维表示棋盘，后一维表示赢法
+	var wins = [];
+	for (var i = 0; i < 15; i++) {
+		wins[i] = [];
+		for (var j = 0; j < 15; j++) {
+			wins[i][j] = [];
+		}
+	}
+	//定义count作为赢法数组的索引，存在第三维
+	var count = 0;
+	//为赢法数组wins填入值（遍历出所有赢法）
+	//所有的横线的赢法
+	for (var i = 0; i < 15; i++) {
+		for (var j = 0; j < 11; j++) {
+			for (var k = 0; k < 5; k++) {
+				wins[i][j + k][count] = true;
+			}
+			count++;
+		}
+	}
+	//所有的竖线的赢法
+	for (var i = 0; i < 11; i++) {
+		for (var j = 0; j < 15; j++) {
+			for (var k = 0; k < 5; k++) {
+				wins[i + k][j][count] = true;
+			}
+			count++;
+		}
+	}
+	//所有的斜线赢法
+	for (var i = 0; i < 11; i++) {
+		for (var j = 0; j < 11; j++) {
+			for (var k = 0; k < 5; k++) {
+				wins[i + k][j + k][count] = true;
+			}
+			count++;
+		}
+	}
+	//所有的反斜线赢法
+	for (var i = 0; i < 11; i++) {
+		for (var j = 14; j > 3; j--) {
+			for (var k = 0; k < 5; k++) {
+				wins[i + k][j - k][count] = true;
+			}
+			count++;
+		}
+	}
+
+
+	//定义赢法统计数组,分为人的统计数组和计算机的统计数组
+	var myWin = [];
+	var computerWin = [];
+	//对数组进行初始化
+	for (var i = 0; i < count; i++) {
+		myWin[i] = 0;
+		computerWin[i] = 0;
+	}
+
+
+
+	//实现点击落子,当落子后对赢法数组进行改变
+	chess.onclick = function(e) {
+		//判断棋是否结束了来决定是否可以继续落子
+		if (over) {
+			return;
+		}
+		//只有在我方下棋时才有效
+		if (!me) {
+			return;
+		}
+		var x = e.offsetX;
+		var y = e.offsetY;
+		var i = Math.floor(x / 30);
+		var j = Math.floor(y / 30);
+		if (chessBoard[i][j] === 0) {
+			drawChessman(i, j, me);
+			//如果是黑棋则记录为1
+			chessBoard[i][j] = 1;
+			//根据落子点，对所有的赢法进行遍历，当这种赢法在此处上是有点存在的时候，为这种赢法的统计数组加1可能
+			for (var k = 0; k < count; k++) {
+				if (wins[i][j][k]) {
+					//当落子点有了一方的点时，另一方需要这处的赢法就不可能实现了，设置为异常值6
+					myWin[k]++;
+					computerWin[k] = 6;
+					if (myWin[k] === 5) {
+						window.alert("你赢了");
+						over = true;
+					}
+				}
+			}
+			if (!over) {
+				//交替下棋
+				me = !me;
+				//落子后进行判断，如果还没有结束就调用computerai函数
+				computerAi();
+			}
+		}
+	};
+
+
+
+	function computerAi() {
+		//定义三个值用来保存最高分的点和该点的坐标
+		var max = 0;
+		var u = 0,
+			v = 0;
+		//定义两个二维数组用来保存每个空缺的点的得分（针对我方和计算机）
+		var myScore = [];
+		var computerScore = [];
+		for (var i = 0; i < 15; i++) {
+			myScore[i] = [];
+			computerScore[i] = [];
+			for (var j = 0; j < 15; j++) {
+				myScore[i][j] = 0;
+				computerScore[i][j] = 0;
+			}
+		}
+		//开始遍历整个棋盘
+		for (var i = 0; i < 15; i++) {
+			for (var j = 0; j < 15; j++) {
+				//如果这个点是空闲的对这个点的得分进行计算
+				if (chessBoard[i][j] === 0) {
+					for (var k = 0; k < count; k++) {
+						//在这个点如果有一种赢法，然后就根据这种赢法的目前成功程度来给这个点打分
+						if (wins[i][j][k]) {
+							//这个点对这种赢法的我方当前的分
+							if (myWin[k] === 1) {
+								myScore[i][j] += 200;
+							} else if (myWin[k] === 2) {
+								myScore[i][j] += 400;
+							} else if (myWin[k] === 3) {
+								myScore[i][j] += 2000;
+							} else if (myWin[k] === 4) {
+								myScore[i][j] += 10000;
+							}
+							//这个点对这种赢法的计算机方当前得分
+							if (computerWin[k] === 1) {
+								computerScore[i][j] += 220;
+							} else if (computerWin[k] === 2) {
+								computerScore[i][j] += 420;
+							} else if (computerWin[k] === 3) {
+								computerScore[i][j] += 2100;
+							} else if (computerWin[k] === 4) {
+								computerScore[i][j] += 20000;
+							}
+						}
+					}
+					if (myScore[i][j] > max) {
+						max = myScore[i][j];
+						u = i;
+						v = j;
+					} else if (myScore[i][j] = max) {
+						if (computerScore[i][j] > computerScore[u][v]) {
+							u = i;
+							v = j;
+						}
+					}
+					if (computerScore[i][j] > max) {
+						max = computerScore[i][j];
+						u = i;
+						v = j;
+					} else if (computerScore[i][j] = max) {
+						if (myScore[i][j] > myScore[u][v]) {
+							u = i;
+							v = j;
+						}
+					}
+				}
+			}
+		}
+
+		//落子
+		drawChessman(u, v, me);
+		chessBoard[u][v] = 2;
+		for (var k = 0; k < count; k++) {
+			if (wins[u][v][k]) {
+				//当落子点有了一方的点时，另一方需要这处的赢法就不可能实现了，设置为异常值6
+				computerWin[k]++;
+				myWin[k] = 6;
+				if (computerWin[k] === 5) {
+					window.alert("计算机赢了");
+					over = true;
+				}
+			}
+		}
+		if (!over) {
+			//交替下棋
+			me = !me;
+		}
+
+	}
+
+
+
+}
